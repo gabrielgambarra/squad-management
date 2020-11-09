a5cff1736261fef7a6d11fa14f215167962b33d2
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-11-09 18:33:41 -0400

    Implement tags input

--- src/pages/TeamForm/TeamForm.js
+++ src/pages/TeamForm/TeamForm.js
@@ -1,9 +1,22 @@
 import './TeamForm.scss';
 import AxiosInstance from '../../providers/services/Api';
 import { SectionContainer, SectionContent, SectionHeader } from '../../shared/styles/SharedStyled';
-import { TeamFormContainer, TeamFormTitle } from './TeamFormStyled';
+import { TeamFormContainer, TeamFormTitle, ChipInputStyled } from './TeamFormStyled';
+import { useState } from 'react';
+import _ from 'underscore';
 
 function TeamForm() {
+
+    const [tags, setTags] = useState([]);
+
+    function addTeamTags(tag) {
+        setTags([...tags, tag]);
+    }
+
+    function deleteTeamTag(tag) {
+        setTags(_.without(tags, tag));
+    }
+
     return (
         <TeamFormContainer>
 
@@ -19,19 +32,19 @@ function TeamForm() {
                         <div className="fields-row">
                             <div className="col">
                                 <div className="input-container">
-                                    <label for="teamName">Team name</label>
+                                    <label htmlFor="teamName">Team name</label>
                                     <input name="teamName" id="teamName" placeholder="Insert team name" />
                                 </div>
 
                                 <div className="input-container">
-                                    <label for="teamDesc">Description</label>
+                                    <label htmlFor="teamDesc">Description</label>
                                     <textarea id="teamDesc" name="teamDesc" rows="9"></textarea>
                                 </div>
                             </div>
 
                             <div className="col">
                                 <div className="input-container">
-                                    <label for="teamWebsite">Team website</label>
+                                    <label htmlFor="teamWebsite">Team website</label>
                                     <input name="teamWebsite" id="teamWebsite" placeholder="http://myteam.com" />
                                 </div>
 
@@ -40,18 +53,24 @@ function TeamForm() {
                                     <div>
                                         <div className="radio-item">
                                             <input type="radio" id="typeRal" name="teamType" value="fantasy" />
-                                            <label for="typeRal">Real</label>
+                                            <label htmlFor="typeRal">Real</label>
                                         </div>
                                         <div className="radio-item">
                                             <input type="radio" id="typeFantasy" name="teamType" value="real" />
-                                            <label for="typeFantasy">Fantasy</label>
+                                            <label htmlFor="typeFantasy">Fantasy</label>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="input-container">
-                                    <label for="teamTags">Tags</label>
-                                    <textarea id="teamTags" name="teamTags" rows="3"></textarea>
+                                    <label htmlFor="teamTags">Tags</label>
+                                    <ChipInputStyled
+                                        id="teamTags"
+                                        disableUnderline
+                                        value={tags}
+                                        onAdd={(chip) => addTeamTags(chip)}
+                                        onDelete={(chip) => deleteTeamTag(chip)}
+                                    />
                                 </div>
                             </div>
                         </div>
--- src/pages/TeamForm/TeamFormStyled.js
+++ src/pages/TeamForm/TeamFormStyled.js
@@ -1,3 +1,4 @@
+import ChipInput from 'material-ui-chip-input';
 import styled from 'styled-components';
 
 export const TeamFormContainer = styled.div`
@@ -14,4 +15,14 @@ export const TeamFormContainer = styled.div`
 export const TeamFormTitle = styled.h4`
     text-transform: uppercase;
     color: #8d8d8d;
+`;
+
+export const ChipInputStyled = styled(ChipInput)`
+    padding: .5em !important;
+    border-radius: 5px;
+    border: 1px solid #dadada !important;
+
+    &:focus-within {
+        border: 1px solid #c74041 !important;
+    }
 `;
